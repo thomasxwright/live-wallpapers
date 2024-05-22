14a4640f46eb34b80357fa9c1471551d515da143
thomasxwright <wright.ptw@gmail.com>
2024-05-22 13:21:42 -0500

    created blooming rings of orbs that blend beautifully using PLUS mode. Also can use gradients to make their insides beautiful, but it kills the framerate

--- app/src/main/java/com/bingle/android/colorful/MyWallpaperService.java
+++ app/src/main/java/com/bingle/android/colorful/MyWallpaperService.java
@@ -1,11 +1,14 @@
 package com.bingle.android.colorful;
 
+import static android.graphics.Color.WHITE;
+
 import android.content.SharedPreferences;
 import android.graphics.BlendMode;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.LinearGradient;
 import android.graphics.Paint;
+import android.graphics.RadialGradient;
 import android.graphics.RectF;
 import android.graphics.Shader;
 import android.os.Handler;
@@ -30,12 +33,10 @@ public class MyWallpaperService extends WallpaperService {
             }
 
         };
-        private final Paint strokePaint = new Paint();
-        private final Paint fillPaint = new Paint();
         private int width;
         private int height;
-        private int ticker;
-        private ThreadGroup threadGroup;
+        private float tick;
+        private final Paint fillPaint = new Paint();
         private boolean visible = true;
         private final boolean touchEnabled;
 
@@ -43,17 +44,11 @@ public class MyWallpaperService extends WallpaperService {
             SharedPreferences prefs = PreferenceManager
                     .getDefaultSharedPreferences(MyWallpaperService.this);
             touchEnabled = prefs.getBoolean("touch", false);
-            ticker = 0;
-
+            tick = 0;
             fillPaint.setAntiAlias(true);
             fillPaint.setStyle(Paint.Style.FILL);
-            fillPaint.setBlendMode(BlendMode.PLUS);
+            fillPaint.setColor(Color.argb(230, 200, 80, 20));
 
-            strokePaint.setAntiAlias(true);
-            strokePaint.setColor(Color.argb(130, 255, 255, 255));
-            strokePaint.setStyle(Paint.Style.STROKE);
-            strokePaint.setStrokeJoin(Paint.Join.ROUND);
-            strokePaint.setStrokeWidth(1f);
             handler.post(drawRunner);
         }
 
@@ -79,7 +74,6 @@ public class MyWallpaperService extends WallpaperService {
                                      int width, int height) {
             this.width = width;
             this.height = height;
-            threadGroup = new ThreadGroup(width);
             super.onSurfaceChanged(holder, format, width, height);
         }
 
@@ -115,7 +109,7 @@ public class MyWallpaperService extends WallpaperService {
             try {
                 canvas = holder.lockCanvas();
                 if (canvas != null) {
-                    advanceItems();
+                    tick += 0.01f;
                     drawItems(canvas);
                 }
             } finally {
@@ -128,24 +122,46 @@ public class MyWallpaperService extends WallpaperService {
             }
         }
 
-        private void advanceItems() {
-            threadGroup.advance();
-        }
-
         // Surface view requires that all elements are drawn completely
         private void drawItems(Canvas canvas) {
-            canvas.drawColor(Color.BLACK);
-            threadGroup.draw(canvas, fillPaint);
-            drawGradient(canvas);
-//                canvas.drawLine(threadXPos, 0, threadXPos + 23, height, paint);
+            canvas.drawColor(Color.rgb(17, 13, 23));
+//            testOverlap(canvas, 400, 200, 4, 75f * (float) (Math.sin(tick)));
+            testOverlap(canvas, 500, 1000, 5, 425f * (float) (Math.sin(tick)));
+        }
 
-//            canvas.drawCircle(ticker, ticker, ticker, paint);
+        private void testOverlap(Canvas canvas, int cx, int cy, int numItems, float distance) {
+            float[][] positions = new float[numItems][2];
+            for (int i = 0; i < numItems; i++) {
+                float multiplier = (float) (i) / numItems;
+                float xPos = cx + (float) (distance * Math.cos(Math.PI * 2 * multiplier));
+                float yPos = cy + (float) (distance * Math.sin(Math.PI * 2 * multiplier));
+                positions[i] = new float[]{xPos, yPos};
+            }
+
+            for (float[] position : positions) {
+                drawGlowOrb(canvas, position[0], position[1], 150);
+            }
+        }
+
+        private void drawGlowOrb(Canvas canvas, float cx, float cy, int radius) {
+            fillPaint.setBlendMode(BlendMode.PLUS);
+            canvas.drawCircle(cx, cy, radius, fillPaint);
+//            drawGlow(canvas, cx, cy, radius * 1.05f, .6f);
+//            drawGlow(canvas, cx + radius * 0.2f, cy + radius * 0.2f, radius * 0.7f, .7f);
+        }
+
+        private void drawGlow(Canvas canvas, float cx, float cy, float radius, float opacity) {
+            Shader shader = new RadialGradient(cx, cy, radius, new int[]{Color.argb((int) (178 * opacity), 255, 255, 255), Color.argb(0, 255, 255, 255)}, new float[]{0.15f, 1f}, Shader.TileMode.CLAMP);
+            fillPaint.setShader(shader);
+            fillPaint.setBlendMode(BlendMode.OVERLAY);
+            canvas.drawRect(new RectF(0, 0, width, height), fillPaint);
+            fillPaint.setShader(null);
         }
 
         private void drawGradient(Canvas canvas) {
 //            Shader shader = new LinearGradient(300, 0, 400, height - 400, Color.argb(0, 232, 12, 130), Color.argb(255, 232, 12, 130), Shader.TileMode.CLAMP);
-            Shader shader = new LinearGradient(300, 0, 400, height - 400,
-                    new int[]{Color.argb(0, 175, 25, 198 ), Color.argb(130, 200, 12, 189), Color.argb(255, 232, 12, 130), Color.argb(255, 232, 12, 130), Color.argb(120, 200, 12, 189), Color.argb(0, 0, 0, 0)},
+            Shader shader = new LinearGradient(300, 0, 400, 2280 - 400,
+                    new int[]{Color.argb(0, 175, 25, 198), Color.argb(130, 200, 12, 189), Color.argb(255, 232, 12, 130), Color.argb(255, 232, 12, 130), Color.argb(120, 200, 12, 189), Color.argb(0, 0, 0, 0)},
                     new float[]{0, 0.5f, 0.75f, 0.8f, 0.81f, 0.82f},
                     Shader.TileMode.CLAMP);
             Paint paint = new Paint();
