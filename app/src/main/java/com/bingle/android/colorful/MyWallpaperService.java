9eedee36bd18e852fe9cfd780066ad87f98325cf
thomasxwright <wright.ptw@gmail.com>
2024-05-29 16:47:05 -0500

    Added fuller set of configurable options, including travel radius, color, movement behavior, speed, and more

--- app/src/main/java/com/bingle/android/colorful/MyWallpaperService.java
+++ app/src/main/java/com/bingle/android/colorful/MyWallpaperService.java
@@ -1,7 +1,5 @@
 package com.bingle.android.colorful;
 
-import static android.graphics.Color.WHITE;
-
 import android.content.SharedPreferences;
 import android.graphics.BlendMode;
 import android.graphics.Canvas;
@@ -14,8 +12,9 @@ import android.graphics.Shader;
 import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.service.wallpaper.WallpaperService;
-import android.view.MotionEvent;
 import android.view.SurfaceHolder;
+
+import java.util.Objects;
 
 public class MyWallpaperService extends WallpaperService {
 
@@ -33,21 +32,49 @@ public class MyWallpaperService extends WallpaperService {
             }
 
         };
+        private int numberOfCircles;
+        private float radiusOfCircles;
+        private float travelRadius;
+        private int alpha;
+        private int red;
+        private int green;
+        private int blue;
+        private String sinOrCos;
+        private Boolean glow;
+        private float horizontalOffset;
+        private float verticalOffset;
         private int width;
         private int height;
+        private float speedMultiplier;
+        private float rotationMultiplier;
+        private float glowOpacityMultiplier;
         private float tick;
         private final Paint fillPaint = new Paint();
         private boolean visible = true;
-        private final boolean touchEnabled;
 
         public MyWallpaperEngine() {
             SharedPreferences prefs = PreferenceManager
                     .getDefaultSharedPreferences(MyWallpaperService.this);
-            touchEnabled = prefs.getBoolean("touch", false);
+
+            numberOfCircles = Integer
+                    .valueOf(prefs.getString("numberOfCircles", "9"));
+            radiusOfCircles = Float.valueOf(prefs.getString("radiusOfCircles", "1f"));
+            travelRadius = Float.valueOf(prefs.getString("travelRadius", "1f"));
+            sinOrCos = prefs.getString("sinOrCos", "neither");
+            glow = prefs.getBoolean("glow", false);
+            speedMultiplier = Float.valueOf(prefs.getString("speedMultiplier", "1f"));
+            horizontalOffset = Float.valueOf(prefs.getString("horizontalOffset", "1f"));
+            verticalOffset = Float.valueOf(prefs.getString("verticalOffset", "1f"));
+            alpha = Integer.valueOf(prefs.getString("alpha", "255"));
+            red = Integer.valueOf(prefs.getString("red", "217"));
+            blue = Integer.valueOf(prefs.getString("blue", "72"));
+            green = Integer.valueOf(prefs.getString("green", "48"));
+            rotationMultiplier = Float.valueOf(prefs.getString("rotationMultiplier", "0f"));
+            glowOpacityMultiplier = Float.valueOf(prefs.getString("glowOpacityMultiplier", "1f"));
             tick = 0;
             fillPaint.setAntiAlias(true);
             fillPaint.setStyle(Paint.Style.FILL);
-            fillPaint.setColor(Color.argb(230, 200, 80, 20));
+            fillPaint.setColor(Color.argb(alpha, red, green, blue));
 
             handler.post(drawRunner);
         }
@@ -77,31 +104,31 @@ public class MyWallpaperService extends WallpaperService {
             super.onSurfaceChanged(holder, format, width, height);
         }
 
-        @Override
-        public void onTouchEvent(MotionEvent event) {
-//            if (touchEnabled) {
-//
-//                float x = event.getX();
-//                float y = event.getY();
-//                SurfaceHolder holder = getSurfaceHolder();
-//                Canvas canvas = null;
-//                try {
-//                    canvas = holder.lockCanvas();
-//                    if (canvas != null) {
-//                        canvas.drawColor(Color.BLACK);
-//                        circles.clear();
-//                        circles.add(new MyPoint(
-//                                String.valueOf(circles.size() + 1), x, y));
-//                        drawCircles(canvas, circles);
-//
-//                    }
-//                } finally {
-//                    if (canvas != null)
-//                        holder.unlockCanvasAndPost(canvas);
-//                }
-//                super.onTouchEvent(event);
-//            }
-        }
+//        @Override
+//        public void onTouchEvent(MotionEvent event) {
+////            if (touchEnabled) {
+////
+////                float x = event.getX();
+////                float y = event.getY();
+////                SurfaceHolder holder = getSurfaceHolder();
+////                Canvas canvas = null;
+////                try {
+////                    canvas = holder.lockCanvas();
+////                    if (canvas != null) {
+////                        canvas.drawColor(Color.BLACK);
+////                        circles.clear();
+////                        circles.add(new MyPoint(
+////                                String.valueOf(circles.size() + 1), x, y));
+////                        drawCircles(canvas, circles);
+////
+////                    }
+////                } finally {
+////                    if (canvas != null)
+////                        holder.unlockCanvasAndPost(canvas);
+////                }
+////                super.onTouchEvent(event);
+////            }
+//        }
 
         private void draw() {
             SurfaceHolder holder = getSurfaceHolder();
@@ -109,7 +136,7 @@ public class MyWallpaperService extends WallpaperService {
             try {
                 canvas = holder.lockCanvas();
                 if (canvas != null) {
-                    tick += 0.01f;
+                    tick += 0.003f * speedMultiplier;
                     drawItems(canvas);
                 }
             } finally {
@@ -126,35 +153,46 @@ public class MyWallpaperService extends WallpaperService {
         private void drawItems(Canvas canvas) {
             canvas.drawColor(Color.rgb(17, 13, 23));
 //            testOverlap(canvas, 400, 200, 4, 75f * (float) (Math.sin(tick)));
-            testOverlap(canvas, 500, 1000, 5, 425f * (float) (Math.sin(tick)));
+            testOverlap(canvas, 500 * horizontalOffset, 1000 * verticalOffset, numberOfCircles, 555f * travelRadius * (float) (Math.sin(tick)));
         }
 
-        private void testOverlap(Canvas canvas, int cx, int cy, int numItems, float distance) {
+        private void testOverlap(Canvas canvas, float cx, float cy, int numItems, float distance) {
             float[][] positions = new float[numItems][2];
             for (int i = 0; i < numItems; i++) {
                 float multiplier = (float) (i) / numItems;
-                float xPos = cx + (float) (distance * Math.cos(Math.PI * 2 * multiplier));
-                float yPos = cy + (float) (distance * Math.sin(Math.PI * 2 * multiplier));
+                float offset = multiplier;
+                if (Objects.equals(sinOrCos, "sin")) {
+                    offset = (float) Math.sin(multiplier + tick * speedMultiplier);
+                }
+                if (Objects.equals(sinOrCos, "cos")) {
+                    offset = (float) Math.cos(multiplier + tick * speedMultiplier);
+                }
+                float rotationOffset = (float) (Math.sin(tick * rotationMultiplier));
+                offset += (float) (Math.cos(tick* rotationMultiplier) );
+                float xPos = cx + (float) (distance * Math.cos(Math.PI * 2 * offset));
+                float yPos = cy + (float) (distance * Math.sin(Math.PI * 2 * offset));
                 positions[i] = new float[]{xPos, yPos};
             }
 
             for (float[] position : positions) {
-                drawGlowOrb(canvas, position[0], position[1], 150);
+                drawGlowOrb(canvas, position[0], position[1], (int) (radiusOfCircles * 150));
             }
         }
 
         private void drawGlowOrb(Canvas canvas, float cx, float cy, int radius) {
             fillPaint.setBlendMode(BlendMode.PLUS);
             canvas.drawCircle(cx, cy, radius, fillPaint);
-//            drawGlow(canvas, cx, cy, radius * 1.05f, .6f);
+            if (glow) {
+                drawGlow(canvas, cx, cy, radius * 1.05f, .8f * glowOpacityMultiplier);
+            }
 //            drawGlow(canvas, cx + radius * 0.2f, cy + radius * 0.2f, radius * 0.7f, .7f);
         }
 
         private void drawGlow(Canvas canvas, float cx, float cy, float radius, float opacity) {
-            Shader shader = new RadialGradient(cx, cy, radius, new int[]{Color.argb((int) (178 * opacity), 255, 255, 255), Color.argb(0, 255, 255, 255)}, new float[]{0.15f, 1f}, Shader.TileMode.CLAMP);
+            Shader shader = new RadialGradient(cx, cy, radius, new int[]{Color.argb((int) ((178 * opacity) > 255 ? 255 : (178 * opacity)), 255, 255, 255), Color.argb(0, 255, 255, 255)}, new float[]{0.15f, 1f}, Shader.TileMode.CLAMP);
             fillPaint.setShader(shader);
             fillPaint.setBlendMode(BlendMode.OVERLAY);
-            canvas.drawRect(new RectF(0, 0, width, height), fillPaint);
+            canvas.drawRect(new RectF(cx - radius, cy - radius, cx + radius, cy + radius), fillPaint);
             fillPaint.setShader(null);
         }
 
